d5b3b0a2eb2f110b37460157cb0771ecab5c790a
Adrien Lacquemant <github@alaq.io>
2021-02-17 23:56:36 -0500

    Switch to redirect

--- functions/scraper.js
+++ functions/scraper.js
@@ -1,54 +1,13 @@
-const chromium = require('chrome-aws-lambda')
-const puppeteer = require('puppeteer-core')
+exports.handler = async (event, context) => {
 
-exports.handler = async (event, context, callback) => {
-  let title = null
-  let browser = null
-  console.log('spawning chrome headless')
-  try {
-    const executablePath = await chromium.executablePath
+  const redirectUrl = 'https://google.com'
 
-    // setup
-    browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: executablePath,
-      headless: chromium.headless,
-    })
-
-    // Do stuff with headless chrome
-    const page = await browser.newPage()
-    const targetUrl = 'https://www.icheckmovies.com/lists/1001+movies+you+must+see+before+you+die/?user=soviel&sort=officialtoplists'
-
-    // Goto page and then do stuff
-    await page.goto(targetUrl, {
-      waitUntil: ["domcontentloaded", "networkidle0"]
-    })
-
-    await page.waitForSelector('#m1824-u5240-movie')
-
-    title = await page.title();
-
-    console.log('done on page', title)
-
-  } catch (error) {
-    console.log('error', error)
-    return callback(null, {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: error
-      })
-    })
-  } finally {
-    // close browser
-    if (browser !== null) {
-      await browser.close()
-    }
+  return {
+    statusCode: 302,
+    headers: {
+      Location: redirectUrl,
+      'Cache-Control': 'no-cache',
+    },
+    body: JSON.stringify({})
   }
-
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      title: title,
-    })
-  })
 }
